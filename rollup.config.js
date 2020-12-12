ee7093997d69774cb4e87d55ea6ba3d3d09f2d7a
Devon Cash <devon.p.cash@gmail.com>
2020-12-12 14:44:55 -0800

    Working on release automation

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 import { terser } from 'rollup-plugin-terser'
-import copy from 'rollup-plugin-cpy';
 
 export default {
   input: './index.js',
   output: [{
     name: 'rollup-plugin-markdown',
-    file: 'bundle/dist/markdown.js',
+    file: 'dist/markdown.js',
     format: 'umd',
     plugins: [terser()],
     globals: {
@@ -13,14 +12,8 @@ export default {
       'rollup-pluginutils': 'rollupPluginutils'
     }
   }, {
-    file: 'bundle/dist/markdown.es.js',
+    file: 'dist/markdown.es.js',
     format: 'es'
   }],
-  plugins: [
-    copy({
-      files: ["package.json", "README.md"],
-      dest: 'bundle'
-    })
-  ],
   external: ['magic-string', 'rollup-pluginutils'],
 };
